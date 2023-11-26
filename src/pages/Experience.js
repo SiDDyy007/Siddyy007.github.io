137da3db44bd84ad8263062c5a36c924f423136f
SiDDyy007 <siddhantp0@gmail.com>
2023-11-26 11:27:03 -0800

    Local changes

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
 import SchoolIcon from "@material-ui/icons/School";
-import WorkIcon from '@mui/icons-material/Work';
+import WorkIcon from '@material-ui/icons/Work';
+
 
 function Experience() {
   return (
